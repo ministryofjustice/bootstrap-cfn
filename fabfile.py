5290669cf872b895e8f8caa43da037b0b74bb5ec
Kyriakos Oikonomakos <kyriakos.oikonomakos@digital.justice.gov.uk>
2015-01-16 10:40:45 +0000

    work in progress

--- fabfile.py
+++ fabfile.py
@@ -15,6 +15,8 @@ env.application = None
 env.environment = None
 env.aws = None
 env.config = None
+TIMEOUT = 3600 
+RETRY_INTERVAL = 10
 
 @task
 def aws(x):
@@ -31,6 +33,20 @@ def application(x):
 @task
 def config(x):
     env.config = str(x).lower()
+
+
+def modify_sg(stack_data, sg_data):
+    groups = []
+    for sg in sg_data:
+        sg_struct = {
+                'Type': 'AWS::EC2::SecurityGroup',
+                'Properties': {
+                    'GroupDescription': 'Auto generated SG',
+                    'SecurityGroupIngress': sg_data[sg]['rules']
+                }
+            }
+        stack_data['Resources'][sg] = sg_struct
+    return stack_data
 
 @task
 def cfn_create():
@@ -50,9 +66,27 @@ def cfn_create():
     # DOWNLOAD BaseHost CFN STACK
     base_stack = json.loads(urllib2.urlopen(env.base_stack_url).read())
     print base_stack
-
     aws_config = AWSConfig(env.aws)
     project_config = ProjectConfig(env.config, env.environment)
-    print project_config.config
+    cfg = project_config.config
 
+    stacks = []
+    for env in cfg[app]:
+        for host, data in cfg[env][EC2_KEY].items():
+            stack_data = modify_sg(base_stack, data['security_groups'])
+            print stack_data
+            stacks.append(create_stack(host, stack_data))
+
+    print 'Waiting for stacks to complete.'
+    attempts = 0
+    while True:
+        if all([stack_done(x) for x in stacks]):
+            break
+        if attempts == TIMEOUT/RETRY_INTERVAL:
+            print '[ERROR] Stack creation timed out'
+            sys.exit(1)
+        attempts += 1
+        time.sleep(RETRY_INTERVAL)
+
+    print 'All stacks completed.'    
 
