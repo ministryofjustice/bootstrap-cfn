3768c710a1358a41ac422e8efc7b154bff9b1b87
Kyriakos Oikonomakos <kyriakos.oikonomakos@digital.justice.gov.uk>
2015-01-16 12:19:20 +0000

    First successful build.
    Added methods in cloudformation class to return status of a build and to
    inject security groups in the template.
    
    Completed fabfile task that creates stacks based on YAML config.

--- awsutils/cloudformation.py
+++ awsutils/cloudformation.py
@@ -11,12 +11,36 @@ class Cloudformation:
         self.config = config
         if self.config.aws_access is not None and self.config.aws_secret is not None:
             self.conn_cfn = boto.cloudformation.connect_to_region(
-                region_name=self.config.region,
+                region_name=self.config.aws_region,
                 aws_access_key_id=self.config.aws_access,
                 aws_secret_access_key=self.config.aws_secret)
         else:
             print "[ERROR] No AWS credentials"
             sys.exit(1)
 
+    def modify_sg(self, stack_data, sg_data):
+        groups = []
+        for sg in sg_data:
+            sg_struct = {
+                    'Type': 'AWS::EC2::SecurityGroup',
+                    'Properties': {
+                        'GroupDescription': 'Auto generated SG',
+                        'SecurityGroupIngress': sg_data[sg]['rules']
+                    }
+                }
+            stack_data['Resources'][sg] = sg_struct
+        return stack_data
+
     def create(self, stack_name, template_body):
         return self.conn_cfn.create_stack(stack_name=stack_name, template_body=template_body)
+
+    def stack_done(self, stack_id):
+        stack_events = self.conn_cfn.describe_stack_events(stack_id)
+        if stack_events[0].resource_type == 'AWS::CloudFormation::Stack'\
+                and stack_events[0].resource_status in ['CREATE_COMPLETE', 'CREATE_FAILED']:
+            return True
+        return False
+
+    def get_last_stack_event(self, stack_id):
+        return self.conn_cfn.describe_stack_events(stack_id)[0]
+
--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,7 @@ import sys
 import urllib2
 import json
 import yaml
+import time
 from fabric.api import env, task, sudo, execute, run, parallel, settings
 from awsutils.cloudformation import Cloudformation
 from helpers.config import AWSConfig, ProjectConfig
@@ -34,20 +35,6 @@ def application(x):
 def config(x):
     env.config = str(x).lower()
 
-
-def modify_sg(stack_data, sg_data):
-    groups = []
-    for sg in sg_data:
-        sg_struct = {
-                'Type': 'AWS::EC2::SecurityGroup',
-                'Properties': {
-                    'GroupDescription': 'Auto generated SG',
-                    'SecurityGroupIngress': sg_data[sg]['rules']
-                }
-            }
-        stack_data['Resources'][sg] = sg_struct
-    return stack_data
-
 @task
 def cfn_create():
     if env.aws is None:
@@ -65,22 +52,23 @@ def cfn_create():
 
     # DOWNLOAD BaseHost CFN STACK
     base_stack = json.loads(urllib2.urlopen(env.base_stack_url).read())
-    print base_stack
     aws_config = AWSConfig(env.aws)
     project_config = ProjectConfig(env.config, env.environment)
     cfg = project_config.config
+    cfn = Cloudformation(aws_config)
 
+    # Inject security groups in stack template and create stacks.
     stacks = []
-    for env in cfg[app]:
-        for host, data in cfg[env][EC2_KEY].items():
-            stack_data = modify_sg(base_stack, data['security_groups'])
-            print stack_data
-            stacks.append(create_stack(host, stack_data))
+    for host, data in cfg['ec2'].items():
+        stack_data = cfn.modify_sg(base_stack, data['security_groups'])
+        stack_name  = '{0}-{1}'.format(host, str(int(time.time())))
+        stacks.append(cfn.create(stack_name, json.dumps(stack_data)))
 
+    # Wait for stacks to complete
     print 'Waiting for stacks to complete.'
     attempts = 0
     while True:
-        if all([stack_done(x) for x in stacks]):
+        if all([cfn.stack_done(x) for x in stacks]):
             break
         if attempts == TIMEOUT/RETRY_INTERVAL:
             print '[ERROR] Stack creation timed out'
@@ -88,5 +76,14 @@ def cfn_create():
         attempts += 1
         time.sleep(RETRY_INTERVAL)
 
-    print 'All stacks completed.'    
+    print 'All stacks completed, checking results.'
+    success_cnt = 0
+    for stack in stacks:
+        stack_evt = cfn.get_last_stack_event(stack)
+        print '{0}: {1}'.format(stack_evt.stack_name,
+                stack_evt.resource_status)
+        if stack_evt.resource_status == 'CREATE_COMPLETE':
+            success_cnt +=1
+    print 'Successfully built {0} out of {0} stacks.'.format(success_cnt,
+            len(stacks))
 
