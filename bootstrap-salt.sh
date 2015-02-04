c295ec202869a6e9acbd09b202e01acfefe311a7
Kyriakos Oikonomakos <kyriakos.oikonomakos@digital.justice.gov.uk>
2015-02-04 12:24:57 +0000

    easy_install, not pip

--- bootstrap-salt.sh
+++ bootstrap-salt.sh
@@ -3,12 +3,12 @@
 # SETUP EC2 INSTANCE
 apt-get update
 apt-get -y install python-setuptools git
-pip install boto
+easy_install boto
 
 mkdir -p /usr/local/bin
 
 easy_install https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz
-wget https://raw.githubusercontent.com/ministryofjustice/bootstrap-cfn/master/ec2_tags.py -O /usr/local/bin
+wget https://raw.githubusercontent.com/ministryofjustice/bootstrap-cfn/master/ec2_tags.py -O /usr/local/bin/ec2_tags.py
 /usr/local/bin/ec2_tags.py
 wget https://raw.githubusercontent.com/saltstack/salt-bootstrap/6080a18e6c7c2d49335978fa69fa63645b45bc2a/bootstrap-salt.sh -O /tmp/bootstrap-salt.sh
 chmod 755 /tmp/bootstrap-salt.sh
