9fabaf57ab7b67989109fba381d6f6bbf358e127
Ash Berlin <ash_github@firemirror.com>
2015-03-24 13:47:38 +0000

    Print errors to stderr, not stdout

--- bootstrap_cfn/errors.py
+++ bootstrap_cfn/errors.py
@@ -1,6 +1,8 @@
+import sys
+
 class BootstrapCfnError(Exception):
     def __init__(self, msg):
-        print "[ERROR] {0}: {1}".format(self.__class__.__name__, msg)
+        print >> sys.stderr,  "[ERROR] {0}: {1}".format(self.__class__.__name__, msg)
 
 class CfnConfigError(BootstrapCfnError):
     pass
