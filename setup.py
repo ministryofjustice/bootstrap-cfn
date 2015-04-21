6bfd7a4394fdb42e2405a346c138cfa751a809a5
Ash Berlin <ash_github@firemirror.com>
2015-04-21 19:08:05 +0100

    v0.3.0

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bootstrap_cfn',
-    version='0.2.2',
+    version='0.3.0',
     url='http://github.com/ministryofjustice/bootstrap_cfn/',
     license='LICENSE',
     author='MOJDS',
