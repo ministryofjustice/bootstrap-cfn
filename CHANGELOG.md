8b721f2ca946d44190e07913e58d9d79a013e2a4
Ash Berlin <ash_github@firemirror.com>
2015-05-07 12:21:35 +0100

    Version 0.4.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,6 @@
-## Version x
+## Version 0.4.0
+
+First release to PyPi
 
 * Add custom ELB healthchecks so you can point an ELB at a URL (not just a port)
 
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bootstrap_cfn',
-    version='0.3.3',
+    version='0.4.0',
     url='http://github.com/ministryofjustice/bootstrap_cfn/',
     license='LICENSE',
     author='MOJDS',
