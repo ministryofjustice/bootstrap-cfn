1ac4034ca4ba98fa18e66ec58192f5e3bb0abfe5
Ash Berlin <ash_github@firemirror.com>
2015-07-10 11:14:48 +0100

    Add stupidly-simple new tests to cover untested files
    These are by no means the best tests, but they are (marginally) better
    than nothing.

---
+++ tests/test_elb.py
@@ -0,0 +1,10 @@
+import unittest
+
+from bootstrap_cfn import elb  # noqa
+
+
+class TestELB(unittest.TestCase):
+
+    def test_loaded(self):
+        # Not a great test, but it at least checks for syntax erros in the file
+        pass
---
+++ tests/test_fab_tasks.py
@@ -0,0 +1,10 @@
+import unittest
+
+from bootstrap_cfn import fab_tasks  # noqa
+
+
+class TestFabTasks(unittest.TestCase):
+
+    def test_loaded(self):
+        # Not a great test, but it at least checks for syntax erros in the file
+        pass
