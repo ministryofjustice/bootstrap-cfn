ed3ae0becd4c1b15be309bca7acdd44f05099198
Niall Creech <niall.creech@digital.justice.gov.uk>
2016-01-21 12:58:34 +0000

    Fix autoscale get_all_groups paging
    The call to boto::autoscale:get_all_groups implements paging,
    this change makes sure that we don't stop at the default 50
    limit, but actually get all the groups.
    
    * Implemenent autoscale:get_all_autoscaling_groups to handle
    pagination of results
    * Update tests so that get_all_groups returns a ResultsSet

--- tests/test_autoscale.py
+++ tests/test_autoscale.py
@@ -4,6 +4,7 @@ import unittest
 
 import boto.ec2.autoscale
 from boto.ec2.autoscale.group import AutoScalingGroup
+from boto.resultset import ResultSet
 
 import mock
 
@@ -12,7 +13,7 @@ from bootstrap_cfn import autoscale
 
 def get_all_groups(names=None, max_records=None, next_token=None):
 
-    groups = []
+    groups = ResultSet()
 
     for i in xrange(1, 3):
         tags = [
@@ -26,7 +27,7 @@ def get_all_groups(names=None, max_records=None, next_token=None):
         asg.name = 'test{0}'.format(i)
         asg.tags = tags
         groups.append(asg)
-
+        groups.next_token = None
     return groups
 
 
