68a71f4833381c5032b824a10a69aee992ad50c7
yufangzhang <yufangzhanguk@gmail.com>
2016-06-29 13:58:01 +0100

    Add a task get_active_stack & README

--- bootstrap_cfn/r53.py
+++ bootstrap_cfn/r53.py
@@ -30,39 +30,37 @@ class R53(object):
         self.conn_r53 = utils.connect_to_aws(boto.route53, self)
 
     def get_hosted_zone_id(self, zone_name):
-        '''
-        Take a zone name
-        Return a zone id or None if no zone found
-        '''
+        """
+        Args:
+            zone_name
+        Returns:
+             a zone id or None if no zone found
+        """
         zone = self.conn_r53.get_hosted_zone_by_name(zone_name)
         if zone:
             zone = zone['GetHostedZoneResponse']['HostedZone']['Id']
             return zone.replace('/hostedzone/', '')
 
     def update_dns_record(self, zone, record, record_type, record_value, is_alias=False, dry_run=False):
-        '''
+        """
         Updates a dns record in route53
-
-        zone -- a string specifying the zone id
-        record -- a string for the record to update
-        record_value -- a string if it is not an alias
-                        a list, if it is an alias, of parameters to pass to
-                        boto record.set_alias() function
-        is_alias -- a boolean to show if record_value is an alias
-        record_type -- a string to specify the record, eg "A"
-
-
+        Args:
+            zone: a string specifying the zone id
+            record: a string for the record to update
+            record_value: a string if it is not an alias
+                            a list, if it is an alias, of parameters to pass to
+                            boto record.set_alias() function
+            is_alias: a boolean to show if record_value is an alias
+            record_type: a string to specify the record, eg "A"
+            dry_run:
         Returns True if update successful or raises an exception if not
-        '''
+        """
         changes = boto.route53.record.ResourceRecordSets(self.conn_r53, zone)
         change = changes.add_change("UPSERT", record, record_type, ttl=60)
         if is_alias:
             # provide list of params as needed by function set_alias
             # http://boto.readthedocs.org/en/latest/ref/route53.html#boto.route53.record.Record.set_alias
-            alias_hosted_zone_id = record_value.alias_hosted_zone_id
-            alias_dns_name = record_value.alias_dns_name
-            alias_evaluate_target_health = record_value.alias_evaluate_target_health
-            change.set_alias(alias_hosted_zone_id, alias_dns_name, alias_evaluate_target_health)
+            change.set_alias(*record_value)
         else:
             change.add_value(record_value)
         if dry_run:
@@ -71,26 +69,27 @@ class R53(object):
             changes.commit()
         return True
 
-    def delete_dns_record(self, zone, record, record_type, record_value, is_alias=False, dry_run=False):
-        '''
+    def delete_dns_record(self, zone_id, record_name, record_type, record_value, is_alias=False, dry_run=False):
+        """
         Delete a dns record in route53
-
-        zone -- a string specifying the zone id
-        record -- a string for the record to update
-        record_type -- a string to specify the record, eg "A"
-
-
-        Returns True if update successful or raises an exception if not
-        '''
-        changes = boto.route53.record.ResourceRecordSets(self.conn_r53, zone)
-        change = changes.add_change("DELETE", record, record_type, ttl=60)
+        Args:
+            zone_id: a string specifying the zone id
+            record_name: a string for the record to update
+            record_value: a string if it is not an alias
+                            a list, if it is an alias, of parameters to pass to
+                            boto record.set_alias() function
+            record_type: a string to specify the record, eg "A"
+            is_alias:
+            dry_run:
+        Returns:
+             True if update successful or raises an exception if not
+        """
+        changes = boto.route53.record.ResourceRecordSets(self.conn_r53, zone_id)
+        change = changes.add_change("DELETE", record_name, record_type, ttl=60)
         if is_alias:
             # provide list of params as needed by function set_alias
             # http://boto.readthedocs.org/en/latest/ref/route53.html#boto.route53.record.Record.set_alias
-            alias_hosted_zone_id = record_value.alias_hosted_zone_id
-            alias_dns_name = record_value.alias_dns_name
-            alias_evaluate_target_health = record_value.alias_evaluate_target_health
-            change.set_alias(alias_hosted_zone_id, alias_dns_name, alias_evaluate_target_health)
+            change.set_alias(*record_value)
         else:
             change.add_value(record_value)
         if dry_run:
@@ -99,16 +98,43 @@ class R53(object):
             res = changes.commit()
         return res
 
-    def get_record(self, zone, zone_id, record, record_type):
-        '''
-        '''
-        fqdn = "{0}.{1}.".format(record, zone)
-        rrsets = self.conn_r53.get_all_rrsets(zone_id, type=record_type, name=fqdn)
+    def get_record(self, zone_name, zone_id, record_name, record_type):
+        """
+
+        Args:
+            zone_name:
+            zone_id:
+            record_name(String):
+            record_type:
+        Returns:
+            String or None, in the event of there being no A or TXT record
+        """
+        record_fqdn = "{0}.{1}.".format(record_name, zone_name)
+        rrsets = self.conn_r53.get_all_rrsets(zone_id, type=record_type, name=record_fqdn)
         for rr in rrsets:
-            if rr.type == record_type and rr.name == fqdn:
+            if rr.type == record_type and rr.name == record_fqdn:
                 if rr.type == 'TXT':
                     rr.resource_records[0] = rr.resource_records[0][1:-1]
                 if rr.type == 'A':
-                    return rr
+                    if rr.alias_dns_name:
+                        return rr.alias_dns_name
                 return rr.resource_records[0]
+        return None
+
+    def get_full_record(self, zone_name, zone_id, record_name, record_type):
+        """
+        Args:
+            zone_name:
+            zone_id:
+            record_name:
+            record_type:
+
+        Returns:
+            RecordObject
+        """
+        record_fqdn = "{0}.{1}.".format(record_name, zone_name)
+        rrsets = self.conn_r53.get_all_rrsets(zone_id, type=record_type, name=record_fqdn)
+        for rr in rrsets:
+            if rr.type == record_type and rr.name == record_fqdn:
+                return rr
         return None
